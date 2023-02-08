755f7ffe25a8189cf7bd78bf16dfcced6fd38fed
cch0524 <stuff532009@gmail.com>
2023-02-08 15:42:03 +0800

    change ip 10.0.1.xxx to 5.1.0.xxx

--- mod/bbb-pads/entrypoint.sh
+++ mod/bbb-pads/entrypoint.sh
@@ -5,9 +5,9 @@ TARGET=/bbb-pads/config/settings.json
 
 cp /bbb-pads/config/settings.json.template $TARGET
 sed -i "s/ETHERPAD_API_KEY/\"$ETHERPAD_API_KEY\"/g" $TARGET
-jq '.etherpad.host = "10.0.1.4"' $TARGET | sponge $TARGET
+jq '.etherpad.host = "5.1.0.4"' $TARGET | sponge $TARGET
 jq '.express.host = "0.0.0.0"' $TARGET | sponge $TARGET
-jq '.redis.host = "10.0.1.5"' $TARGET | sponge $TARGET
+jq '.redis.host = "5.1.0.5"' $TARGET | sponge $TARGET
 
 
 cd /bbb-pads
