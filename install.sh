bcab4571b7bff7333445f18c8348f2d4e3ea0df5
RogerLo-2000 <ss37462609734@gmail.com>
2023-03-24 17:07:13 +0800

    clean

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# kubectl delete configMap https-proxy-config
+
 kubectl create configmap mongodb-config --from-file=mongodb-config
 kubectl create configmap https-proxy-config --from-file=https-proxy-config
 kubectl apply -f ./yaml --recursive
