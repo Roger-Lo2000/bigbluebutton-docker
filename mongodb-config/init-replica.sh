0d509fa6ce0ce9e5ddf724bbd3551b75ffdb1c8e
RogerLo-2000 <ss37462609734@gmail.com>
2023-03-24 17:32:49 +0800

    change mongo init

--- mongodb-config/init-replica.sh
+++ mongodb-config/init-replica.sh
@@ -1,26 +1,13 @@
 #!/bin/sh
-set -e
-
-
-host=${HOSTNAME:-$(hostname -f)}
-
-# shut down again
+echo "start init"
 mongod --pidfilepath /tmp/docker-entrypoint-temp-mongod.pid --shutdown
-# restart again binding to 0.0.0.0 to allow a replset with 10.7.7.6
+
 mongod --oplogSize 8 --replSet rs0 --noauth \
-   --config /tmp/docker-entrypoint-temp-config.json \
+   --config /etc/mongo/mongod.conf \
    --bind_ip 0.0.0.0 --port 27017 \
    --tlsMode disabled \
    --logpath /proc/1/fd/1 --logappend \
-   --pidfilepath /tmp/docker-entrypoint-temp-mongod.pid --fork
+   --pidfilepath /tmp/docker-entrypoint-temp-mongod.pid
+mongo $mongo_pod_IP --eval "rs.initiate({ _id: 'rs0', members: [{ _id: 0, host: '$mongo_pod_IP:27017'}]})"
 
-# init replset with defaults
-# mongo 172.17.0.7 --eval "rs.initiate({
-#    _id: 'rs0',
-#    members: [ { _id: 0, host: '172.17.0.7:27017' } ]
-# })"
-
-echo "Waiting to become a master"
-echo 'while (!db.isMaster().ismaster) { sleep(100); }' | mongo
-
-echo "I'm the master!"
+echo "init done"
