be8c72debd5e1cb6568322f3ee7247992af620f4
RogerLo-2000 <ss37462609734@gmail.com>
2023-03-24 19:42:54 +0800

    kubectl get pod

--- mongodb-config/init-replica.sh
+++ mongodb-config/init-replica.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "start init"
+echo "set"
 sleep 3
 mongod --oplogSize 8 --replSet rs0 --noauth \
    --config /data/config/mongod.conf \
@@ -7,6 +7,8 @@ mongod --oplogSize 8 --replSet rs0 --noauth \
    --tlsMode disabled \
    --logpath /proc/1/fd/1 --logappend \
    --pidfilepath /tmp/docker-entrypoint-temp-mongod.pid
+sleep 3
+echo "init"
 mongo $mongo_pod_IP --eval "rs.initiate({ _id: 'rs0', members: [{ _id: 0, host: '$mongo_pod_IP:27017'}]})"
 
 echo "init done"
