e1b8e4cee3a2ff1efc28a47729e4a5fb7da97db3
RogerLo-2000 <ss37462609734@gmail.com>
2023-03-29 21:11:42 +0800

    change freeswitch

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 # kubectl delete configMap https-proxy-config
 # kubectl delete configMap mongodb-config
 # kubectl delete --all deployment --namespace=default
+# kubectl delete pvc --all
 kubectl create configmap mongodb-config --from-file=mongodb-config
 kubectl create configmap https-proxy-config --from-file=https-proxy-config
+kubectl create configmap freeswitch-config1 --from-file=freeswtich-config1
+kubectl create configmap freeswitch-config2 --from-file=freeswtich-config2
 kubectl apply -f ./yaml --recursive
