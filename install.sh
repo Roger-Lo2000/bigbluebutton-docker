638d70e5a059182e7ba7d9dd6b528b7eb3350ee3
RogerLo-2000 <ss37462609734@gmail.com>
2023-03-29 22:13:32 +0800

    comment mount of periodic

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ kubectl create configmap https-proxy-config --from-file=https-proxy-config
 kubectl create configmap freeswitch-config1 --from-file=freeswitch-config1
 kubectl create configmap freeswitch-config2 --from-file=freeswitch-config2
 kubectl apply -f ./yaml --recursive
+
+
+# delete all deployment pvc pv
+kubectl delete --all deployment --namespace=default \
+kubectl delete pvc --all \
+kubectl delete pv --all \
