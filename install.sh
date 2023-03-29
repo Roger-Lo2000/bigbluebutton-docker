877ccae820642747d29325e33993cfbe1a189de7
RogerLo-2000 <ss37462609734@gmail.com>
2023-03-29 11:15:28 +0800

    comment volumeMount of periodic

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # kubectl delete configMap https-proxy-config
 # kubectl delete configMap mongodb-config
+# kubectl delete --all deployment --namespace=default
 kubectl create configmap mongodb-config --from-file=mongodb-config
 kubectl create configmap https-proxy-config --from-file=https-proxy-config
 kubectl apply -f ./yaml --recursive
