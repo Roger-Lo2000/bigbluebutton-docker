b1e1653f7531201c9cff6e331c6cc7c241c7f70f
RogerLo-2000 <ss37462609734@gmail.com>
2023-04-05 20:20:43 +0800

    test for pv-pvc filestore

--- install.sh
+++ install.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-# kubectl delete configMap https-proxy-config
-# kubectl delete configMap mongodb-config
-# kubectl delete --all deployment --namespace=default
-# kubectl delete pvc --all
-# kubectl delete pv --all
+kubectl delete configMap https-proxy-config
+kubectl delete configMap mongodb-config
+kubectl delete --all deployment --namespace=default
+kubectl delete pvc --all
+kubectl delete pv --all
+
+
 kubectl create configmap mongodb-config --from-file=mongodb-config
 kubectl create configmap https-proxy-config --from-file=https-proxy-config
 kubectl create configmap freeswitch-config1 --from-file=freeswitch-config1
 kubectl create configmap freeswitch-config2 --from-file=freeswitch-config2
 kubectl apply -f ./yaml --recursive
 
-
-# delete all deployment pvc pv
-kubectl delete --all deployment --namespace=default \
-kubectl delete pvc --all \
-kubectl delete pv --all 
