8a5e6aef1fc874ba686e4118c77a0f42df4489c5
RogerLo-2000 <ss37462609734@gmail.com>
2023-04-20 17:38:44 +0800

    remove webhook.nginx

--- install.sh
+++ install.sh
@@ -16,5 +16,8 @@ kubectl create configmap https-proxy-config --from-file=https-proxy-config
 kubectl create configmap freeswitch-config1 --from-file=freeswitch-config1
 kubectl create configmap freeswitch-config2 --from-file=freeswitch-config2
 kubectl create configmap nginx-config --from-file=nginx-config
+kubectl delete configMap nginx-config
+kubectl delete deployment nginx
+
 kubectl apply -f ./yaml --recursive
 
