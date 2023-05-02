c3024507762e3775114ba59f82b6dea9017bad9a
RogerLo-2000 <ss37462609734@gmail.com>
2023-05-02 11:10:11 +0800

    use freeswtich cluster ip webrtc-sfu

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ kubectl delete pv --all
 kubectl get pods --all-namespaces | grep ContainerStatusUnknown | awk '{print $2 " --namespace=" $1}' | xargs kubectl delete pod
 kubectl get pods --all-namespaces | grep Evicted | awk '{print $2 " --namespace=" $1}' | xargs kubectl delete pod
 kubectl get pods --all-namespaces | grep Error | awk '{print $2 " --namespace=" $1}' | xargs kubectl delete pod
+kubectl get pods --all-namespaces | grep Complete | awk '{print $2 " --namespace=" $1}' | xargs kubectl delete pod
 
 kubectl get endpoints ##入口
 
@@ -20,16 +21,3 @@ kubectl delete configMap nginx-config
 kubectl delete deployment nginx
 
 kubectl apply -f ./yaml --recursive
-
-iptables -t nat -A PREROUTING -p tcp -d 35.229.151.245 --dport 8080 \
- -j DNAT --to-destination 172.17.0.1:8080
-
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 \
--j DNAT --to-destination 172.17.0.1:8080
-
-sudo iptables -t nat -A PREROUTING -i ens4 -p tcp --dport 8080 -j DNAT --to-destination 172.17.0.1:8080
-route add -net 172.17.0.1 netmask 255.255.255.255 metric 1024 ens4
-adds a route to the local network 192.56.76.x via "eth0".  The word "dev" can be omitted here.
-iptables -t nat -I PREROUTING --src 0/0 --dst 172.17.0.1 -p tcp --dport 80 -j REDIRECT --to-ports 80
-iptables -A PREROUTING -t nat -i eth0:0 -p tcp --dport 80 -j DNAT --to 172.17.0.1:80
-iptables -A FORWARD -p tcp -d 172.17.0.1 --dport 80 -j ACCEPT
